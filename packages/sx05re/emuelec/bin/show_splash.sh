fd3513716887a90c100862d2e42031bf04c0fc6a
worstcase-scenario <83265453+worstcase-scenario@users.noreply.github.com>
2025-04-20 22:03:57 +0200

    Update show_splash.sh
    Updated the wrongfully deleted part.

--- packages/sx05re/emuelec/bin/show_splash.sh
+++ packages/sx05re/emuelec/bin/show_splash.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019-present SumavisionQ5 (https://github.com/SumavisionQ5)
+# Modifications by Shanti Gilbert (https://github.com/shantigilbert)
+
+# 12/07/2019 use mpv for all splash 
+# 19/01/2020 use ffplay for all splash 
+# 06/02/2020 move splash to roms folder and add global splash support
 
 . /etc/profile
 
