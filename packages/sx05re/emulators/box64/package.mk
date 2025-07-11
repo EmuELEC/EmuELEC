9ac56e34abed9b5b49728e081f350de66f26808f
worstcase-scenario <iamspamalot@gmx.de>
2025-07-11 09:25:42 +0200

    Updated box68 and box64 to latest commit

--- packages/sx05re/emulators/box64/package.mk
+++ packages/sx05re/emulators/box64/package.mk
@@ -2,7 +2,8 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="box64"
-PKG_VERSION="2b300bd199a7a65a3de1eecd24d6dff5593a9b55"
+#PKG_VERSION="2b300bd199a7a65a3de1eecd24d6dff5593a9b55"
+PKG_VERSION="78460724ea0770f4c5b85ec117273d4a3d54b230"
 
 # amlogic-old uses older linux headers not compatible with newer versions
 [ "${DEVICE}" == "Amlogic-old" ] && PKG_VERSION="6392550208eadf07419692920acc2955bb844af7"
