aa3faa2c265c82aa5fb1ee56dadc07edcc295ced
Joshua L <Langerz82@users.noreply.github.com>
2025-10-01 02:15:49 +1000

    initial commit.

--- packages/sx05re/emuelec/bin/setres.sh
+++ packages/sx05re/emuelec/bin/setres.sh
@@ -44,7 +44,7 @@ switch_resolution()
 
   # Here we first clear the primary display buffer of leftover artifacts then set
   # the secondary small buffers flag to stop copying across.
-  blank_buffer >> /dev/null
+#  blank_buffer >> /dev/null
 
   case ${MODE} in
     480cvbs|576cvbs|480p*|480i*|576p*|720p*|1080p*|1440p*|2160p*|576i*|720i*|1080i*|1440i*|2160i*|*x*)
@@ -159,10 +159,6 @@ fi
 FBW=0
 FBH=0
 
-# Here we first clear the primary display buffer of leftover artifacts then set
-# the secondary small buffers flag to stop copying across.
-blank_buffer >> /dev/null
-
 # The current display mode before it may get changed below.
 OLD_MODE=$( cat ${FILE_MODE} )
 
@@ -174,7 +170,8 @@ BUFF=$(get_ee_setting ee_video_fb1_size)
 [[ -z "${BUFF}" ]] && BUFF=32
 
 if [[ -n "${BUFF}" ]] && [[ ${BUFF} > 0 ]]; then
-  fbset -fb /dev/fb1 -g ${BUFF} ${BUFF} ${BUFF} ${BUFF} ${BPP}
+	[[ "${max_fb}" == 0 ]] && fbset -fb /dev/fb1 -g ${BUFF} ${BUFF} ${BUFF} ${BUFF} ${BPP}
+	[[ "${max_fb}" == 1 ]] && fbset -fb /dev/fb0 -g ${BUFF} ${BUFF} ${BUFF} ${BUFF} ${BPP}
 fi
 
 # This is needed to reset scaling.
@@ -223,11 +220,12 @@ fi
 # Once we know the Width and Height is valid numbers we set the primary display
 # buffer, and we multiply the 2nd height by a factor of 2 I assume for interlaced 
 # support.
-CURRENT_MODE=$( cat ${FILE_MODE} )
-if [[ "${CURRENT_MODE}" == "${MODE}" ]]; then
+CURRENT_SIZE="$( fbset -fb /dev/fb${max_fb} | grep geometry | cut -d' ' -f2-3 )"
+NEW_SIZE="${FBW} ${FBH}"
+if [[ "${CURRENT_SIZE}" != "${NEW_SIZE}" ]]; then
+	blank_buffer >> /dev/null
   echo "SET MAIN FRAME BUFFER"
   set_main_framebuffer ${FBW} ${FBH} 
-  blank_buffer
 fi
 
 # Now that the primary buffer has been acquired we blank it again because the new
