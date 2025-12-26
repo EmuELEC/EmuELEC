7bfbed1f970663801eccf82f246ca2c1ade90e8a
skiiingray <ray.yj.chang@gmail.com>
2025-12-26 18:31:25 +0800

    Keep local edits for fbneo and np2kai after branch sync

--- packages/sx05re/emulators/openbor/openborff/package.mk
+++ packages/sx05re/emulators/openbor/openborff/package.mk
@@ -23,11 +23,10 @@ else
 	PKG_PATCH_DIRS="emuelec-aarch64"
 fi
 
-
 pre_configure_target() {
   PKG_MAKE_OPTS_TARGET="BUILD_LINUX_${ARCH}=1 \
                         -C ${PKG_BUILD}/engine \
-                        SDKPATH="${SYSROOT_PREFIX}"
+                        SDKPATH=\"${SYSROOT_PREFIX}\" \
                         PREFIX=${TARGET_NAME}"
 }
 
