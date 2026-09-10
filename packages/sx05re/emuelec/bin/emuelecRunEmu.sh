69d33b67eb6ab661dbb1e668e3fdf7c3b90f987a
worstcase-scenario <83265453+worstcase-scenario@users.noreply.github.com>
2026-09-10 20:45:29 +0200

    Updated Xroar to 1.11 (and other new features) (#1653)
    * Update xroar package version and configuration
    
    * Implement compressed file handling in xroar.sh
    
    Add support for handling compressed ROM files (ZIP and 7Z) in xroar.sh, including extraction and identification of valid XRoar files.
    
    * Add new file extensions for xroar machines to es_systems.json
    
    * updated Xroar standalone emulator to v. 1.11 / changed sources / updated starting script to include .zip files and more supported file types
    
    * Updated script to work with sidecar .xr files
    
    * Updated starting script
    
    * deleted old script
    
    * remove xroar launch script
    
    * remove xroar launch script
    
    * Simplified entry for xroar systems

--- packages/sx05re/emuelec/bin/emuelecRunEmu.sh
+++ packages/sx05re/emuelec/bin/emuelecRunEmu.sh
@@ -404,30 +404,12 @@ case ${PLATFORM} in
             RUNTHIS='${TBASH} memustart.sh "${ROMNAME}"'
         fi
 		;;	
-        "dragon32"|"dragon64")
-			if [ "${EMU}" = "xroar" ]; then
-			set_kill_keys "xroar.aarch64"
-			RUNTHIS='${TBASH} /usr/bin/xroar.sh "${ROMNAME}"'
-		fi
-		;;
-		"coco")
-			if [ "${EMU}" = "xroar" ]; then
-			set_kill_keys "xroar.aarch64"
-            RUNTHIS='${TBASH} /usr/bin/xroar.sh "${ROMNAME}"'
-		fi
-		;;
-		"coco3")
-			if [ "${EMU}" = "xroar" ]; then
-			set_kill_keys "xroar.aarch64"
-		    RUNTHIS='${TBASH} /usr/bin/xroar.sh "${ROMNAME}"'
-		fi
-		;;
-		"mc10")
-			if [ "${EMU}" = "xroar" ]; then
-			set_kill_keys "xroar.aarch64"
-            RUNTHIS='${TBASH} /usr/bin/xroar.sh "${ROMNAME}"'
-		fi
-		;;
+		"dragon32"|"dragon64"|"mc10"|"coco"|"coco3")
+        if [ "${EMU}" = "xroar" ]; then
+            set_kill_keys "xroar"
+            RUNTHIS='${TBASH} /usr/bin/xroarstart.sh "${ROMNAME}"'
+        fi
+     	;;
 		"saturn")
         if [ "${EMU}" = "yabasanshiroSA" ]; then
             set_kill_keys "yabasanshiro"
